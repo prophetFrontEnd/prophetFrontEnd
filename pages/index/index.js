2f11fbab6745b0c20f63f19fdaf476d97836591b
huanghe <987933671@qq.com>
2022-02-22 01:37:37 +0800

    index

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,111 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
+// pages/demo/demo.js
 Page({
+
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
+    agree:'agree',
     userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
+
+  showtips(){
+    wx.showToast({
+      title: '请勾选用户协议',
+      icon: 'none',
+      duration: 1000
     })
   },
-  onLoad() {
-    if (wx.getUserProfile) {
+
+  checkboxChange(){
+    if(this.data.agree == 'agree'){
       this.setData({
-        canIUseGetUserProfile: true
+        agree: ''
+      })
+      wx.showToast({
+        title: '您已取消勾选用户协议',
+        icon: 'none',
+        duration: 1000
+      })
+    }else{
+      this.setData({
+        agree: 'agree'
       })
     }
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+  
+  login(){
     wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      desc: '用于登录账号，完善身份信息',
       success: (res) => {
-        console.log(res)
         this.setData({
           userInfo: res.userInfo,
-          hasUserInfo: true
         })
+        console.log('success',res);
       }
     })
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+
+  toprotocol(){
+    wx.navigateTo({
+      url: '/pages/agreement/agreement'
     })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
